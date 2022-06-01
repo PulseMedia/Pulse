dab6f895fc527dc14d573f431eccadc35da0e4a2
Tom <47848626+Pulse-Git@users.noreply.github.com>
2022-06-01 22:13:20 +0200

    initial release

--- api/index.js
+++ api/index.js
@@ -4,10 +4,13 @@ import { representations } from "./representations.js";
 import { api } from "./api.js";
 import { allowedBrowserApis } from "./browserApi.js";
 
+import { langFlags } from "../i18n/flags.js";
+
 export default {
   map,
   representations,
   casts,
   api,
-  allowedBrowserApis
+  allowedBrowserApis,
+  langFlags
 }
