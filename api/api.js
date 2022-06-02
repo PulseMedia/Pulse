edefca55ace4a6f747013fd5dc1a32868bb9556b
Tom <47848626+Pulse-Git@users.noreply.github.com>
2022-06-02 17:08:52 +0200

    initial release

--- api/api.js
+++ api/api.js
@@ -1,11 +1,11 @@
-import Pulse from "./api/pulse/Pulse.js";
+import App from "./api/app/App.js";
 import VFS from "./api/vfs/VFS.js";
 import Text from "./api/text/Text.js";
 import NET from "./api/net/NET.js";
 import Media from "./api/media/Media.js";
 
 export const api = {
-  "Pulse": Pulse.api,
+  "App": App.api,
   "VFS": VFS.api,
   "Text": Text.api,
   "NET": NET.api,
--- api/map.js
+++ api/map.js
@@ -1,4 +1,4 @@
-import Pulse from "./api/pulse/Pulse.js";
+import App from "./api/app/App.js";
 import VFS from "./api/vfs/VFS.js";
 import Text from "./api/text/Text.js";
 import NET from "./api/net/NET.js";
@@ -6,7 +6,7 @@ import Media from "./api/media/Media.js";
 import internal from "./api/internal.js";
 
 export const map = {
-  "Pulse": Pulse.map,
+  "App": App.map,
   "VFS": VFS.map,
   "Text": Text.map,
   "Media": Media.map,
